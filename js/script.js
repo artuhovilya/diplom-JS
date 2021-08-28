34f53a7d696966fd57e61d106d7078c266b14293
artuhovilya <artuhovilya@gmail.com>
2021-08-28 21:12:54 +0500

    Добавление новых данных в проект

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+
+var ageInput = document.getElementById('age');
+var heightInput = document.getElementById('height');
+var weightInput = document.getElementById('weight');
+var submitButton = document.querySelector('.form__submit-button');
+var resetButton = document.querySelector('.form__reset-button');
+var activityRadioButtons = document.getElementsByName('activity');
+var genderRadioButtons = document.getElementsByName('gender');
+var summaryBlock = document.querySelector('section');
+var inputElement = document.getElementsByClassName('input__wrapper');
